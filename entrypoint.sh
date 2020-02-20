088f6b1e8dd04c3483b1812075cd87fa1c1e60d1
Alex Stansfield <alex.stansfield@gmail.com>
2020-02-20 09:06:17 +0700

    Use the config file instead

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,5 @@ fi
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-php /usr/local/bin/phpcs.phar --standard=${INPUT_STANDARD} --extensions=php --report=checkstyle -q ${INPUT_TARGET_DIRECTORY} \
+php /usr/local/bin/phpcs.phar --report=checkstyle \
   | reviewdog -name=PHP_CodeSniffer -f=checkstyle -reporter=${INPUT_REPORTER} -level=${INPUT_LEVEL} -diff='git diff'
